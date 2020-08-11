8212d1cfa3f01e6d0e0614b9e6e889735832b5e8
Tetiana <tetiana.osadcha@mckesson.com>
2020-08-11 12:21:22 +0300

    Set up webpack

---
+++ src/components/TestComponent/TestComponent.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class TestComponent extends Component {
+    render() {
+        return <div><h2 className='title'> React Movies Application</h2></div>;
+    }
+}
